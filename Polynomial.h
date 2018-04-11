e0c21773533585ff086361a47609e523c73fd020
LucienShui <18952904@qq.com>
2018-04-12 01:16:33 +0800

    第三次上机作业

--- Polynomial.h
+++ Polynomial.h
@@ -10,83 +10,85 @@
 #include <algorithm>
 
 struct Polynomial {
-    int *data; // coefficient
-    int maxsize, upper;
+    struct Node {
+        int coe, index;
+        Node *next;
+        Node(int coe = -1, int index= -1, Node *next = nullptr):coe(coe), index(index), next(next) {}
+        void clear() { // 递归删除整个链表
+            if (next) next->clear();
+            delete this;
+        }
+    } *head = nullptr; // coefficient
+    int size = 0;
 
-    explicit Polynomial(int maxn = 1007) {
-        maxsize = maxn, upper = -1;
-        data = new int[maxsize + 7];
-        memset(data, 0, sizeof(data));
+    Polynomial() { head = new Node; }
+
+    ~Polynomial() { head->clear(); }
+
+    void insert(int coe, int index) { // 如果能找到同次方的就将系数加和，否则新建一个节点
+        if (!head) head = new Node;
+        Node *cur = head;
+        while (cur->next && cur->next->index < index) cur = cur->next; // 找到第一个系数大于等于index的节点的前一个节点
+        if (cur->next) {
+            if (cur->next->index == index) cur->next->coe += coe; // 如果键值相等
+            else {
+                Node *tmp = cur->next;
+                cur->next = new Node(coe, index, tmp);
+                size++;
+            }
+        } else cur->next = new Node{coe, index}, size++; // 如果已经到了末尾
     }
 
-    Polynomial(Polynomial &pol) {
-        upper = pol.upper;
-        maxsize = pol.maxsize;
-        data = new int[maxsize + 7];
-        memcpy(data, pol.data, sizeof(pol.data));
+    Polynomial(Polynomial &tmp) { // 拷贝函数直接暴力n^2插入，毕竟作业，忽略复杂度
+        for (Node *cur = tmp.head->next; cur; cur = cur->next) insert(cur->coe, cur->index);
     }
 
-    void update(int coe, int index) {
-        data[index] = coe;
-        upper = std::max(index, upper);
-    }
-
-    Polynomial operator + (Polynomial &tmp) { // 多项式加法
-        Polynomial ret(std::max(tmp.maxsize, maxsize));
-        ret.upper = std::max(tmp.upper, upper);
-        for (int i = 0; i <= ret.upper; i++) ret.data[i] = data[i] + tmp.data[i];
+    Polynomial operator + (Polynomial &tmp) { // 多项式加法，利用insert函数的性质，直接n^2暴力插入就可实现功能
+        Polynomial ret = *this;
+        for (Node *cur = tmp.head->next; cur; cur = cur->next) ret.insert(cur->coe, cur->index);
         return ret;
     }
 
     Polynomial operator ~ () const { // 求导
-        Polynomial ret(maxsize);
-        ret.upper = upper - 1;
-        for (int i = 1; i <= upper; i++) ret.data[i - 1] = data[i] * i;
+        Polynomial ret;
+        for (Node *cur = head->next; cur; cur = cur->next) if (cur->index) ret.insert((cur->coe) * (cur->index), cur->index - 1);
         return ret;
     }
 
     friend std::ostream & operator << (std::ostream &os, const Polynomial &tmp) {
-        if (tmp.upper < 1) os << tmp.upper ? 0 : tmp.data[0];
+        if (tmp.size < 1) os << 0;
         else {
-            int cur = 0;
-            while (tmp.data[cur] == 0 && cur <= tmp.upper) cur++;
-            if (cur > tmp.upper) os << 0;
-            else {
-                bool flag = false;
-                for (int coe; cur <= tmp.upper; cur++)
-                    if (tmp.data[cur]) {
-                        coe = tmp.data[cur];
-                        if (flag && coe > 0) os << '+';
-                        flag = true;
-                        if (abs(coe) == 1) {
-                            if (coe < 0) os << '-';
-                        } else os << coe;
-                        if (cur) os << 'x';
-                        if (cur > 1) os << '^' << cur;
-                    }
+            Node *cur = tmp.head->next;
+            bool flag = false;
+            for (int coe, index; cur; cur = cur->next) {
+                if (!(coe = cur->coe)) continue;
+                index = cur->index;
+                if (flag && coe > 0) os << '+';
+                flag = true;
+                if (abs(coe) == 1) {
+                    if (coe < 0) os << '-';
+                } else os << coe;
+                if (index) os << 'x';
+                if (index > 1) os << '^' << index;
             }
         }
         return os;
     }
 
-    long long calculate(int num) {
-        long long ret = 0, base = 1;
-        for (int i = 0; i <= upper; i++, base *= num) ret += data[i] * base;
+    long long qpow(int p, int q) {
+        long long ret = 1;
+        while (q) {
+            if (q & 1) ret *= p;
+            p *= p, q >>= 1;
+        }
         return ret;
     }
-} a, b;
+
+    long long calculate(int num) {
+        long long ret = 0;
+        for (Node *cur = head->next; cur; cur = cur->next) ret += cur->coe * qpow(num, cur->index);
+        return ret;
+    }
+};
 
 #endif //POLYNOMIAL_H
-
-using namespace std;
-int main() {
-//    freopen("in.txt", "r", stdin);
-    int n, coe, num;
-    for (cin >> n; n--; ) cin >> coe >> num, a.update(coe, num);
-    for (cin >> n; n--; ) cin >> coe >> num, b.update(coe, num);
-    cout << "A(x)+B(x)=" << a + b << endl;
-    cout << "A'(x)=" << ~a << endl;
-    cin >> num;
-    cout << "A(" << num << ")=" << a.calculate(num) << endl;
-    return 0;
-}
